75bfa95e73b0d7d9bf87f021bf25395b7c0b8672
hoantd <hoantd@coccoc.com>
2022-11-16 13:54:01 +0700

    groovy

---
+++ groovy.groovy
@@ -0,0 +1,32 @@
+/*
+INF-6381
+
+
+This jobs is allowing developer to execute manually command to container in kubernetes -dev namespace 
+This is also a testing playground before we make one to PROD
+
+*/
+
+projMap = ["namespace": "${NAMESPACE}", "project": "${PROJ}", "overlays": OVERLAYS.split()]
+command = 
+
+changelog = CHANGELOG.trim().replaceAll("\n+", "\n");
+
+currentBuild.displayName = "#" + currentBuild.number + " - " + NAMESPACE+":"+PROJ+":"+OVERLAYS.split()
+
+
+stage("Pre-check") {
+
+}
+
+stage("Execute") {
+
+}
+
+stage("Check service") {
+
+}
+
+stage("Finish") {
+
+}
